59886a2b84c898b7fa665b70c7f48604233aced6
ChangHee Park <arrans9591@gmail.com>
2024-01-02 18:45:13 +0900

    라우터 정상작동 확인하기

--- pages/boards/[boardId]/index.js
+++ pages/boards/[boardId]/index.js
@@ -2,6 +2,9 @@ import { useRouter } from "next/router";
 
 export default function BoardPages() {
   const router = useRouter();
+  console.log(router);
+  console.log("라우터 정상 작동 확인 : " + router.query.boardId);
+
   return (
     <>
       <div>게시글아이디 : {router.query.boardId}</div>
