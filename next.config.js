fc8ee125342c2b5e2c4e8ecdaee7bfe8c6baef39
ChangHee Park <arrans9591@gmail.com>
2024-01-02 19:17:28 +0900

    내용 수정!

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "export",
+  // output: "export",
+  // output: "standalone",
   trailingSlash: true,
   generateBuildId: () => "appletrick-develop-project",
   // 아래 주소들만 out 폴더에 만들어주는 것 => getServerSideProps 있는 페이지는 제외
