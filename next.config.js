a475d627badb9c175591e1d9f8f82d4d8fa29ef2
ChangHee Park <arrans9591@gmail.com>
2024-01-02 19:01:52 +0900

    분기 배포 설정

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // output: "export",
+  output: "export",
   trailingSlash: true,
+  generateBuildId: () => "appletrick-develop-project",
+  // 아래 주소들만 out 폴더에 만들어주는 것 => getServerSideProps 있는 페이지는 제외
+  exportPathMap: () => ({
+    "/": { page: "/" },
+    "/boards": { page: "/boards" },
+    "/404": { page: "/404" },
+  }),
 };
 
 module.exports = nextConfig;
--- pages/boards/[boardId]/index.js
+++ pages/boards/[boardId]/index.js
@@ -12,3 +12,8 @@ export default function BoardPages() {
     </>
   );
 }
+
+export const getServerSideProps = () => {
+  // 만약 서버사이드 렌더링을 하는 페이지라면 => out 폴더 생성 불가
+  //  => 이런 경우 next.config.js 에서 exportPathMap으로 현제 페이지 제외시키기
+};
