bcf834c2f3b20b535966757ca237a43eaf90226a
ChangHee Park <arrans9591@gmail.com>
2024-01-03 20:43:42 +0900

    assetprefix 추가

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   trailingSlash: true,
   reactStrictMode: true,
   generateBuildId: () => "appletrick-develop-project",
-  // assetPrefix: "/static/",
+  assetPrefix: "/static/",
   // output: "export",
   // 아래 주소들만 out 폴더에 만들어주는 것 => getServerSideProps 있는 페이지는 제외
   // exportPathMap: () => ({
